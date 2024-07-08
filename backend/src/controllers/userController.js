be9e05e9210d6977f51efef9f6131a808bb63e58
janparacha <parmusta@gmail.com>
2024-07-08 08:45:29 +0200

    delet error message and adapt middleware

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -7,7 +7,7 @@ class userController {
   static async getUsers(req, res) {
     const { data, error } = await crudService.findAll(User, req.query);
     if (error) {
-      return res.status(400).json({ error: error.message });
+      return res.status(400);
     }
     res.json({ users: data });
   }
@@ -17,13 +17,13 @@ class userController {
     if (error) {
       return next(error);
     }
-    res.status(201).json({ success: true, user: data });
+    res.status(201);
   }
 
   static async getUser(req, res) {
     const { data, error } = await crudService.findByPk(User, req.params.id);
     if (error) {
-      return res.status(404).json({ error: error.message });
+      return res.status(404);
     }
     res.status(200).json({ user: data });
   }
@@ -31,7 +31,7 @@ class userController {
   static async deleteUser(req, res) {
     const { data, error } = await crudService.destroy(User, req.params.id);
     if (error) {
-      return res.status(404).json({ error: error.message });
+      return res.status(404);
     }
     res.sendStatus(204);
   }
@@ -39,38 +39,38 @@ class userController {
   static async replaceUser(req, res) {
     const { data: deletedData, error: deleteError } = await crudService.destroy(User, req.params.id);
     if (deleteError) {
-      return res.status(404).json({ error: deleteError.message });
+      return res.status(404);
     }
     const { data, error } = await crudService.create(User, req.body);
     if (error) {
-      return res.status(400).json({ error: error.message });
+      return res.status(400);
     }
-    res.status(deletedData ? 200 : 201).json({ user: data });
+    res.status(deletedData ? 200 : 201);
   }
 
   static async modifyUsers(req, res) {
     const { data, error } = await crudService.update(User, req.params.id, req.body);
     if (error) {
-      return res.status(404).json({ error: error.message });
+      return res.status(404);
     }
     res.json({ user: data });
   }
   
   static async login(req, res) {
     if (Object.keys(req.body).length > 2) {
-        return res.status(400).json({ message: 'Invalid request' });
+        return res.status(400);
     }
 
     try {
         const user = await User.findOne({ where: { email: req.body.email } });
 
         if (!user) {
-            return res.status(401).json({ message: 'Authentication failed. User not found.' });
+            return res.status(401);
         }
 
         const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
         if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Authentication failed. Wrong password.' });
+            return res.status(401);
         }
 
         const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
@@ -81,17 +81,9 @@ class userController {
             maxAge: 3600000 // 1 heure
         });
 
-        return res.status(200).json({ 
-            message: 'Authentication successful', 
-            user: {
-                id: user.id,
-                email: user.email,
-                firstname: user.firstname,
-                lastname: user.lastname
-            } 
-        });
+        return res.status(200).json();
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error', error: error.message });
+        return res.status(500).json();
     }
 }
 
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const validate = require("../middlewares/validate.js");
-const registerVerif = require("../middlewares/registerVerif.js");
+const addRoleUser = require("../middlewares/addRoleUser.js");
 const modifySchema = require("../schema/modifySchema.js");
 const registerSchema = require("../schema/registerSchema.js");
 const userController = require("../controllers/userController.js");
@@ -10,8 +10,8 @@ const userRouter = Router();
 userRouter.get("/users", userController.getUsers);
 userRouter.get("/users/:id", userController.getUser);
 userRouter.delete("/users/:id", userController.deleteUser);
-userRouter.put("/users/:id", validate(registerSchema), registerVerif(), userController.replaceUser);
+userRouter.put("/users/:id", validate(registerSchema), addRoleUser(), userController.replaceUser);
 userRouter.patch("/users/:id", validate(modifySchema), userController.modifyUsers);
-userRouter.post("/register", validate(registerSchema), registerVerif(),userController.register);
+userRouter.post("/register", validate(registerSchema), addRoleUser(),userController.register);
     
 module.exports = userRouter;

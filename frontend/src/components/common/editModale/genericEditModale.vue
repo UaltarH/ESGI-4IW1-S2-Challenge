b482df9c24169830b9546395c371b2a199e928bb
nikolasbaele <nikolasbaele@gmail.com>
2024-07-12 15:21:08 +0200

    ajout d'une modale generique pour edit user

---
+++ frontend/src/components/common/editModale/genericEditModale.vue
@@ -0,0 +1,59 @@
+<template>
+    <DialogContent class="overflow-auto h-[90vh]">
+        <DialogHeader>
+            <DialogTitle class="text-xl font-semibold">Édition d'un élément</DialogTitle>
+            <DialogDescription class="text-gray-600 mt-2">
+                Modifiez les champs de l'élément sélectionné ci-dessous.
+            </DialogDescription>
+        </DialogHeader>
+        <form @submit.prevent="onSubmit" class="flex flex-col h-full">
+            <div v-for="(value, key) in item" :key="key" class="m-5">
+                <FormField v-if="key !== 'id'" :name="key" v-slot="{ componentField }">
+                    <FormItem>
+                        <FormLabel :for="key" class="block text-sm font-medium text-gray-700">{{ key }}</FormLabel>
+                        <FormControl>
+                            <Input
+                                v-bind="componentField"
+                                :id="key"
+                                v-model="item[key]"
+                                type="text"
+                                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            />
+                        </FormControl>
+                    </FormItem>
+                </FormField>
+            </div>
+            <div class="w-full flex justify-center items-center mt-6">
+                <DialogClose as-child>
+                    <Button type="submit" class="bg-indigo-500 text-white hover:bg-indigo-600 focus:ring-indigo-500">Sauvegarder</Button>
+                </DialogClose>
+            </div>
+        </form>
+    </DialogContent>
+</template>
+
+<script lang="ts" setup>
+import { reactive, defineProps, defineEmits, watch } from 'vue';
+import { DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogClose } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { FormControl, FormItem, FormLabel, FormField } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const props = defineProps({
+    model: Object
+});
+
+const emits = defineEmits(['close', 'save']);
+
+const item = reactive({ ...props.model });
+
+// Optional: Watch for changes in props.model to update item reactively
+watch(() => props.model, (newVal) => {
+    Object.assign(item, newVal);
+}, { deep: true });
+
+const onSubmit = () => {
+    emits('save', item);
+    emits('close');
+};
+</script>

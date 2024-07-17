dcc8ecf7ba0e04f10c62a9378d599c27c38476fd
janparacha <parmusta@gmail.com>
2024-07-17 10:53:06 +0200

    CI/CD fonctionnelle

--- backend/tests/integration/controllers/userController.test.js
+++ backend/tests/integration/controllers/userController.test.js
@@ -1,19 +1,30 @@
 const userController = require("../../../src/controllers/userController");
 const crudService = require("../../../src/services/crudGeneric");
-const User = require("../../../src/sequelize/models/User");
+const { User } = require("../../../src/sequelize/models/");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 jest.mock("../../../src/services/crudGeneric");
 jest.mock("bcryptjs");
 jest.mock("jsonwebtoken");
+jest.mock("../../../src/sequelize/models/", () => ({
+  User: {
+    findOne: jest.fn(),
+  },
+}));
 
 describe("userController", () => {
+  const mockUUID = "123e4567-e89b-12d3-a456-426614174000";
+  
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe("getUsers", () => {
     it("should return users data", async () => {
       const req = { query: {} };
       const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
-      const mockUsers = [{ id: 1, name: "John Doe", email: "test@test.fr", password: "Password123!" }];
+      const mockUsers = [{ id: mockUUID, name: "John Doe", email: "test@test.fr" }];
 
       crudService.findAll.mockResolvedValue({ data: mockUsers, error: null });
 
@@ -24,7 +35,7 @@ describe("userController", () => {
 
     it("should return 400 if there is an error", async () => {
       const req = { query: {} };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const res = { status: jest.fn().mockReturnThis() };
 
       crudService.findAll.mockResolvedValue({ data: null, error: new Error("Error") });
 
@@ -36,11 +47,11 @@ describe("userController", () => {
 
   describe("register", () => {
     it("should register a new user", async () => {
-      const req = { body: { name: "John Doe" } };
+      const req = { body: { name: "John Doe", email: "john@example.com", password: "password123" } };
       const res = { sendStatus: jest.fn() };
       const next = jest.fn();
 
-      crudService.create.mockResolvedValue({ data: { id: 1, name: "John Doe" }, error: null });
+      crudService.create.mockResolvedValue({ data: { id: mockUUID, ...req.body }, error: null });
 
       await userController.register(req, res, next);
 
@@ -62,32 +73,44 @@ describe("userController", () => {
 
   describe("getUser", () => {
     it("should return user data", async () => {
-      const req = { params: { id: 1 } };
+      const req = { params: { id: mockUUID }, query: {} };
       const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
 
-      crudService.findByPk.mockResolvedValue({ data: { id: 1, name: "John Doe" }, error: null });
+      crudService.findByPk.mockResolvedValue({ data: { id: mockUUID, name: "John Doe" }, error: null });
 
       await userController.getUser(req, res);
 
       expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.json).toHaveBeenCalledWith({ user: { id: 1, name: "John Doe" } });
+      expect(res.json).toHaveBeenCalledWith({ user: { id: mockUUID, name: "John Doe" } });
     });
 
     it("should return 404 if user is not found", async () => {
-      const req = { params: { id: 1 } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const req = { params: { id: mockUUID }, query: {} };
+      const res = { status: jest.fn().mockReturnThis() };
 
-      crudService.findByPk.mockResolvedValue({ data: null, error: new Error("Error") });
+      crudService.findByPk.mockResolvedValue({ data: null, error: new Error("User not found") });
 
       await userController.getUser(req, res);
 
       expect(res.status).toHaveBeenCalledWith(404);
     });
+
+    it("should return specific fields if requested", async () => {
+      const req = { params: { id: mockUUID }, query: { fields: "name,email" } };
+      const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+
+      User.findOne = jest.fn().mockResolvedValue({ id: mockUUID, name: "John Doe", email: "john@example.com" });
+
+      await userController.getUser(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({ user: { id: mockUUID, name: "John Doe", email: "john@example.com" } });
+    });
   });
 
   describe("deleteUser", () => {
     it("should delete a user", async () => {
-      const req = { params: { id: 1 } };
+      const req = { params: { id: mockUUID } };
       const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
       crudService.destroy.mockResolvedValue({ data: true, error: null });
@@ -95,13 +118,14 @@ describe("userController", () => {
       await userController.deleteUser(req, res);
 
       expect(res.status).toHaveBeenCalledWith(204);
+      expect(res.json).toHaveBeenCalledWith({ user: true });
     });
 
     it("should return 404 if user is not found", async () => {
-      const req = { params: { id: 1 } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const req = { params: { id: mockUUID } };
+      const res = { status: jest.fn().mockReturnThis() };
 
-      crudService.destroy.mockResolvedValue({ data: null, error: new Error("Error") });
+      crudService.destroy.mockResolvedValue({ data: null, error: new Error("User not found") });
 
       await userController.deleteUser(req, res);
 
@@ -111,13 +135,10 @@ describe("userController", () => {
 
   describe("deleteMultiplesUsers", () => {
     it("should delete multiple users", async () => {
-      const req = { body: { usersId: "1,2,3" } };
-      const res = { sendStatus: jest.fn(), status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const req = { body: { usersId: `${mockUUID},${mockUUID}` } };
+      const res = { sendStatus: jest.fn() };
 
-      crudService.destroy
-        .mockResolvedValueOnce({ data: true, error: null })
-        .mockResolvedValueOnce({ data: true, error: null })
-        .mockResolvedValueOnce({ data: true, error: null });
+      crudService.destroy.mockResolvedValue({ data: true, error: null });
 
       await userController.deleteMultiplesUsers(req, res);
 
@@ -125,10 +146,10 @@ describe("userController", () => {
     });
 
     it("should return 500 if there is an error", async () => {
-      const req = { body: { usersId: "1,2,3" } };
-      const res = { sendStatus: jest.fn(), status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const req = { body: { usersId: `${mockUUID},${mockUUID}` } };
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
 
-      crudService.destroy.mockResolvedValueOnce({ data: null, error: new Error("Error") });
+      crudService.destroy.mockRejectedValue(new Error("Deletion error"));
 
       await userController.deleteMultiplesUsers(req, res);
 
@@ -139,22 +160,22 @@ describe("userController", () => {
 
   describe("replaceUser", () => {
     it("should replace a user", async () => {
-      const req = { params: { id: 1 }, body: { name: "John Doe" } };
-      const res = { status: jest.fn().mockReturnThis() };
+      const req = { params: { id: mockUUID }, body: { name: "John Doe" } };
+      const res = { sendStatus: jest.fn() };
 
       crudService.destroy.mockResolvedValue({ data: true, error: null });
-      crudService.create.mockResolvedValue({ data: { id: 1, name: "John Doe" }, error: null });
+      crudService.create.mockResolvedValue({ data: { id: mockUUID, name: "John Doe" }, error: null });
 
       await userController.replaceUser(req, res);
 
-      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.sendStatus).toHaveBeenCalledWith(200);
     });
 
     it("should return 404 if user is not found for deletion", async () => {
-      const req = { params: { id: 1 }, body: { name: "John Doe" } };
+      const req = { params: { id: mockUUID }, body: { name: "John Doe" } };
       const res = { status: jest.fn().mockReturnThis() };
 
-      crudService.destroy.mockResolvedValue({ data: null, error: new Error("Error") });
+      crudService.destroy.mockResolvedValue({ data: null, error: new Error("User not found") });
 
       await userController.replaceUser(req, res);
 
@@ -162,11 +183,11 @@ describe("userController", () => {
     });
 
     it("should return 400 if user cannot be created", async () => {
-      const req = { params: { id: 1 }, body: { name: "John Doe" } };
+      const req = { params: { id: mockUUID }, body: { name: "John Doe" } };
       const res = { status: jest.fn().mockReturnThis() };
 
       crudService.destroy.mockResolvedValue({ data: true, error: null });
-      crudService.create.mockResolvedValue({ data: null, error: new Error("Error") });
+      crudService.create.mockResolvedValue({ data: null, error: new Error("Creation error") });
 
       await userController.replaceUser(req, res);
 
@@ -176,21 +197,21 @@ describe("userController", () => {
 
   describe("modifyUsers", () => {
     it("should modify a user", async () => {
-      const req = { params: { id: 1 }, body: { name: "John Doe" } };
-      const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+      const req = { params: { id: mockUUID }, body: { name: "John Doe" } };
+      const res = { sendStatus: jest.fn() };
 
-      crudService.update.mockResolvedValue({ data: { id: 1, name: "John Doe" }, error: null });
+      crudService.update.mockResolvedValue({ data: { id: mockUUID, name: "John Doe" }, error: null });
 
       await userController.modifyUsers(req, res);
 
-      expect(res.json).toHaveBeenCalledWith({ user: { id: 1, name: "John Doe" } });
+      expect(res.sendStatus).toHaveBeenCalledWith(204);
     });
 
     it("should return 404 if user is not found", async () => {
-      const req = { params: { id: 1 }, body: { name: "John Doe" } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const req = { params: { id: mockUUID }, body: { name: "John Doe" } };
+      const res = { status: jest.fn().mockReturnThis() };
 
-      crudService.update.mockResolvedValue({ data: null, error: new Error("Error") });
+      crudService.update.mockResolvedValue({ data: null, error: new Error("User not found") });
 
       await userController.modifyUsers(req, res);
 
@@ -201,60 +222,59 @@ describe("userController", () => {
   describe("login", () => {
     it("should login a user and return a token", async () => {
       const req = { body: { email: "test@example.com", password: "password" } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn(), cookie: jest.fn() };
+      const res = { sendStatus: jest.fn(), cookie: jest.fn() };
 
-      User.findOne = jest.fn().mockResolvedValue({ id: 1, email: "test@example.com", password: "hashedpassword" });
+      User.findOne.mockResolvedValue({ id: mockUUID, email: "test@example.com", password: "hashedpassword", role: "user" });
       bcrypt.compare.mockResolvedValue(true);
       jwt.sign.mockReturnValue("token");
 
       await userController.login(req, res);
 
-      expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.json).toHaveBeenCalled();
+      expect(res.sendStatus).toHaveBeenCalledWith(200);
       expect(res.cookie).toHaveBeenCalledWith("auth_token", "token", expect.any(Object));
     });
 
     it("should return 400 if too many fields are provided", async () => {
       const req = { body: { email: "test@example.com", password: "password", extra: "extra" } };
-      const res = { status: jest.fn().mockReturnThis() };
+      const res = { sendStatus: jest.fn() };
 
       await userController.login(req, res);
 
-      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.sendStatus).toHaveBeenCalledWith(400);
     });
 
     it("should return 401 if user is not found", async () => {
       const req = { body: { email: "test@example.com", password: "password" } };
-      const res = { status: jest.fn().mockReturnThis() };
+      const res = { sendStatus: jest.fn() };
 
-      User.findOne = jest.fn().mockResolvedValue(null);
+      User.findOne.mockResolvedValue(null);
 
       await userController.login(req, res);
 
-      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.sendStatus).toHaveBeenCalledWith(401);
     });
 
     it("should return 401 if password is invalid", async () => {
       const req = { body: { email: "test@example.com", password: "password" } };
-      const res = { status: jest.fn().mockReturnThis() };
+      const res = { sendStatus: jest.fn() };
 
-      User.findOne = jest.fn().mockResolvedValue({ id: 1, email: "test@example.com", password: "hashedpassword" });
+      User.findOne.mockResolvedValue({ id: mockUUID, email: "test@example.com", password: "hashedpassword" });
       bcrypt.compare.mockResolvedValue(false);
 
       await userController.login(req, res);
 
-      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.sendStatus).toHaveBeenCalledWith(401);
     });
 
     it("should return 500 if there is a server error", async () => {
       const req = { body: { email: "test@example.com", password: "password" } };
-      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+      const res = { sendStatus: jest.fn() };
 
-      User.findOne = jest.fn().mockRejectedValue(new Error("Server error"));
+      User.findOne.mockRejectedValue(new Error("Server error"));
 
       await userController.login(req, res);
 
-      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.sendStatus).toHaveBeenCalledWith(500);
     });
   });
 });
